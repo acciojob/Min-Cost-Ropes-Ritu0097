550479fc7b7ac6ff577ed49ac213d9c4074f40cb
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-16 23:28:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    const pq = [...arr];
+    pq.sort((a, b) => a - b);
+    let cost = 0;
+    while (pq.length > 1) {
+        const first = pq.shift();
+        const second = pq.shift();
+        const combined = first + second;
+        cost += combined;
+        pq.push(combined);
+        pq.sort((a, b) => a - b);
+    }
+    return cost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
